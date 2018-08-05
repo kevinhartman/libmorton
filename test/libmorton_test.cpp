27b630545797c43efea0609ad83397a84b774d66
Kevin Hartman <kevin@hart.mn>
2018-08-05 00:58:34 -0400

    Fix comment on 2D decode TODO.

--- test/libmorton_test.cpp
+++ test/libmorton_test.cpp
@@ -196,7 +196,7 @@ int main(int argc, char *argv[]) {
 	cout << "++ Checking 2D methods for correctness" << endl;
 	check2D_EncodeCorrectness<uint_fast64_t, uint_fast32_t>(f2D_64_encode);
 	check2D_EncodeCorrectness<uint_fast32_t, uint_fast16_t>(f2D_32_encode);
-	// TODO: check 2D zdecode for correctness
+	// TODO: check 2D decode for correctness
 	
 	cout << "++ Running each performance test " << times << " times and averaging results" << endl;
 	for (int i = 128; i <= 512; i = i * 2){
